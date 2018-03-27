fed80c298468f8b09943fbe95f64b0f5776968e4
Dallas Hall <dallas.hall@gmail.com>
2018-03-26 22:52:36 -0700

    Testing some ideas

--- entry.js
+++ entry.js
@@ -3,5 +3,32 @@
 // All of the Node.js APIs are available in this process.
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:8000');
+let defaultState = {
+  count: 1
+};
+
+let state;
+
+const socket = io('http://localhost:8000', {
+  query: {
+    room: 'cool-room1',
+  }
+});
 console.log('loaded');
+socket.on('update', (newState) => {
+  state = newState.count ? newState : defaultState;
+  console.log(state);
+});
+
+const nameBtn = document.getElementById('name-btn');
+nameBtn.addEventListener('click', (e) => {
+  const nameInput = document.getElementById('name-input');
+  const name = nameInput.value;
+  console.log(name);
+});
+
+const updateStateBtn = document.getElementById('update-state-btn');
+updateStateBtn.addEventListener('click', (e) => {
+  state.count++;
+  socket.emit('update', { count: state.count });
+});

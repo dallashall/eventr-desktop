8392ff894b27751498c1a4f68fd37da54d7b715d
Dallas Hall <dallas.hall@gmail.com>
2018-04-02 23:15:44 -0700

    Roughed in user status tracking

--- app/components/main.js
+++ app/components/main.js
@@ -2,6 +2,7 @@ import io from 'socket.io-client';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setUISocket } from '../actions/ui';
+import dispatchAction from '../actions/socket';
 
 class Main extends Component {
   constructor(props) {
@@ -14,24 +15,21 @@ class Main extends Component {
       user,
       event,
       setUISocket,
+      dispatchAction,
     } = this.props;
     const socket = io('http://localhost:8000', {
       query: {
-        room: event,
-        user
-      }
+        payload: JSON.stringify({
+          room: event || 'test',
+          user
+        }),
+      },
     });
     setUISocket(socket);
     socket.on(`hydrateUser:${user.id}`, (newState) => {
       console.log('Hydrating with: ', newState);
     });
-
-    // const nameBtn = document.getElementById('name-btn');
-    // nameBtn.addEventListener('click', (e) => {
-    //   const nameInput = document.getElementById('name-input');
-    //   const name = nameInput.value;
-    //   console.log(name);
-    // });
+    socket.on('action', action => dispatchAction(action));
 
     // const updateStateBtn = document.getElementById('update-state-btn');
     // updateStateBtn.addEventListener('click', (e) => {
@@ -43,26 +41,34 @@ class Main extends Component {
   render() {
     const {
       user,
-      event
+      event,
+      users,
     } = this.props;
     console.log('main', this.props)
     return (
       <div className="container">
         <p>{user.userName}</p>
-        <p>{event}</p>
+        <p>event: {event}</p>
+        <ul>
+        {
+          users.ids.map(id => <li key={id}>{users.byId[id].userName}</li>)
+        }
+        </ul>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ ui }) => ({
+const mapStateToProps = ({ ui, entities }) => ({
   user: ui.user,
   event: ui.event,
   socket: ui.socket,
+  users: entities.users,
 });
 
 const mapDispatchToProps = {
-  setUISocket
+  setUISocket,
+  dispatchAction,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
--- app/reducers/entities/users/index.js
+++ app/reducers/entities/users/index.js
@@ -1,9 +1,29 @@
+import {
+  RECEIVE_USER,
+  REMOVE_USER,
+} from '../../../actions/users';
 
 const defaultState = {
   byId: {},
   ids: [],
 };
 
-export default (state = defaultState, { type, payload }) => {
-  return state;
+export default (users = defaultState, { type, payload }) => {
+  switch (type) {
+    case RECEIVE_USER:
+      return {
+        byId: { ...users.byId, [payload.id]: payload },
+        ids: [...users.ids, payload.id],
+      };
+    case REMOVE_USER: {
+      const newUsers = { 
+        byId: { ...users.byId },
+      };
+      delete newUsers.byId[payload.id];
+      newUsers.ids = Object.keys(newUsers.byId);
+      return newUsers;
+    }
+    default:
+      return users;
+  }
 };
